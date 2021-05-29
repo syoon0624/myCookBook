24478c915b5d941e080a6915ecd93d625cefb391
이승윤 <syoon0624@khu.ac.kr>
2021-05-29 23:35:50 +0900

    feat: categori detail 쿼리 추가 기능

--- routes/categori.js
+++ routes/categori.js
@@ -1,31 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var CategoriModel = require('../models/CategoriModel');
-//var csrf = require('csurf');
-//var csrfProtection = csrf({ cookie: true });
+var VideoModel = require('../models/VideoModel');
+var Youtube = require('youtube-node');
+var youtube = new Youtube();
+
 var loginRequired = require('../libs/loginRequired');
 
-var path = require('path');
-var uploadDir = path.join(__dirname, '../uploads'); // 루트의 uploads위치에 저장한다.
-var fs = require('fs');
-
-//multer 셋팅
-var multer = require('multer');
-var storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-    //이미지가 저장되는 도착지 지정
-    callback(null, uploadDir);
-  },
-  filename: function (req, file, callback) {
-    // products-날짜.jpg(png) 저장
-    callback(
-      null,
-      'products-' + Date.now() + '.' + file.mimetype.split('/')[1]
-    );
-  },
-});
-
-var upload = multer({ storage: storage });
+var limit = 10; // 출력 갯수
+youtube.setKey('AIzaSyCAaeW1qMSInEdN1OzU20FZlToIZYkb1bc'); // API 키 입력
 
 router.get('/', function (req, res) {
   res.send('categori main page');
@@ -43,7 +26,6 @@ router.get('/products', function (req, res) {
 });
 
 router.get('/categories/write', loginRequired, function (req, res) {
-  //edit에서도 같은 form을 사용하므로 빈 변수( product )를 넣어서 에러를 피해준다
   res.render('category/form', { categories: '' });
 });
 
@@ -67,15 +49,83 @@ router.post('/categories/write', loginRequired, function (req, res) {
 
 router.get('/products/detail/:id', function (req, res) {
   //url 에서 변수 값을 받아올떈 req.params.id 로 받아온다
+  var word = req.query.keyword;
   CategoriModel.findOne({ _id: req.params.id }, function (err, product) {
+    var video = [];
     //제품정보를 받고 그안에서 댓글을 받아온다.
     CategoriModel.find({ product_id: req.params.id }, function (err, comments) {
-      res.render('category/productsDetail', {
-        product: product,
-        comments: comments,
-      });
+      if (word != null) {
+        var count = 0;
+        youtube.addParam('order', 'rating'); // 평점 순으로 정렬
+        youtube.addParam('type', 'video'); // 타입 지정
+        youtube.addParam('videoLicense', 'creativeCommon'); // 크리에이티브 커먼즈 아이템만 불러옴
+        youtube.search(word, limit, function (err, result) {
+          // 검색 실행
+          console.log(word);
+          if (err) {
+            console.log(err);
+          } // 에러일 경우 에러공지하고 빠져나감
+          //console.log(JSON.stringify(result, null, 2)); // 받아온 전체 리스트 출력
+          var items = result['items']; // 결과 중 items 항목만 가져옴
+          for (var i in items) {
+            var it = items[i];
+            for (var j in it) {
+              if (it[j]['title'] != null) {
+                var title = it[j]['title'];
+              }
+              if (it[j]['videoId'] != null) {
+                var video_id = it[j]['videoId'];
+              }
+              var urls = 'https://www.youtube.com/watch?v=' + video_id;
+            }
+            var item = {
+              id: count,
+              title: title,
+              video_id: video_id,
+              urls: urls,
+              categori: product.title,
+            };
+            count++;
+            video.push(item);
+          }
+          res.render('category/productsDetail', {
+            product: product,
+            comments: comments,
+            videos: video,
+          });
+        });
+      } else {
+        res.render('category/productsDetail', {
+          product: product,
+          comments: comments,
+          videos: video,
+        });
+      }
     });
   });
+});
+
+router.post('/products/detail/:id', loginRequired, function (req, res) {
+  var item = [];
+  var count = 1;
+  for (var i in req.body.videoNum) {
+    item.push(req.body.videoNum[i].split('///'));
+    var video = new VideoModel({
+      categori: item[i][2],
+      id: count,
+      title: item[i][1],
+      video_id: item[i][3],
+      urls: item[i][4],
+    });
+    var validationError = video.validateSync();
+    if (validationError) {
+      res.send(validationError);
+    } else {
+      video.save(function (err) {});
+    }
+    count++;
+  }
+  res.redirect('/categori/products');
 });
 
 router.get('/products/edit/:id', loginRequired, function (req, res) {
@@ -87,37 +137,24 @@ router.get('/products/edit/:id', loginRequired, function (req, res) {
   });
 });
 
-router.post(
-  '/products/edit/:id',
-  loginRequired,
-  upload.single('thumbnail'),
-  //  csrfProtection,
-  function (req, res) {
-    //그전에 지정되 있는 파일명을 받아온다
-    CategoriModel.findOne({ _id: req.params.id }, function (err, product) {
-      //아래의 코드만 추가되면 된다.
-      if (req.file && product.thumbnail) {
-        //요청중에 파일이 존재 할시 이전이미지 지운다.
-        fs.unlinkSync(uploadDir + '/' + product.thumbnail);
+router.post('/products/edit/:id', loginRequired, function (req, res) {
+  //그전에 지정되 있는 파일명을 받아온다
+  CategoriModel.findOne({ _id: req.params.id }, function (err, product) {
+    var query = {
+      name: req.body.name,
+      thumbnail: req.file ? req.file.filename : product.thumbnail,
+      price: req.body.price,
+      description: req.body.description,
+    };
+    CategoriModel.update(
+      { id: req.params.id },
+      { $set: query },
+      function (err) {
+        res.redirect('/categori/products/detail/' + req.params.id);
       }
-      //위의 코드만 추가되면 된다.
-      //넣을 변수 값을 셋팅한다
-      var query = {
-        name: req.body.name,
-        thumbnail: req.file ? req.file.filename : product.thumbnail,
-        price: req.body.price,
-        description: req.body.description,
-      };
-      CategoriModel.update(
-        { id: req.params.id },
-        { $set: query },
-        function (err) {
-          res.redirect('/category/products/detail/' + req.params.id);
-        }
-      );
-    });
-  }
-);
+    );
+  });
+});
 
 router.get('/products/delete/:id', function (req, res) {
   CategoriModel.deleteMany({ _id: req.params.id }, function (err) {
